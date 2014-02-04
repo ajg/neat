d07d2d9704814e82f154bc4b9ccabf1b200a5840
Alvaro J. Genial <genial@alva.ro>
2014-02-04 15:01:32 -0500

    Remove cruft

--- Text/Neat/Input/Django.hs
+++ Text/Neat/Input/Django.hs
@@ -33,25 +33,11 @@ file path = File path <$> block <* eof where
     Switch  <$> tag "switch" Value    <*> cases <*> default' <* end "endswitch",
     With    <$> tag "with"   binding  <*> block              <* end "endwith",
     Text    <$> some textChar]
- -- Text    <$> (filterText <$> some textChar <*> precedesBare)]
 
   else'    = optionMaybe . try $ end "else" *> block
   default' = optionMaybe . try $ end "default" *> block
   cases    = spaces *> many (try case') where
     case'  = Case <$> tag "case" Pattern <*> block
-
-  {-
-  filterText chars True = chars
-  filterText chars False = trimTrail chars
-
-  trimTrail s | hasTrail = intercalate "\n" init'
-              | otherwise = s where
-    (lines', init', last') = (lines s, init lines', last lines')
-    hasTrail = length lines' > 1 && not (null last') && all isSpace last'
-
-  precedesBare = option False (True <$ lookAhead bare)
-    where bare = try $ string $ fst bareMarkers
-  -}
 
   tag name f = let t = keyword name *> trimmedText
                 in f <$> location <*> (t `within` elementMarkers)
